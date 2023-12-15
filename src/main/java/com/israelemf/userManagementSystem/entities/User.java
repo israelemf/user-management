82e337b373b65f62b55a79c67f10a740797f05c4
Israel Meloso Filho <israelemf@outlook.com>
2023-12-15 20:20:07 -0300

    created User Entity

---
+++ src/main/java/com/israelemf/userManagementSystem/entities/User.java
@@ -0,0 +1,36 @@
+package com.israelemf.userManagementSystem.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(length = 100)
+    private String email;
+    @Column(unique = true)
+    private String login;
+    private String password;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
